43a76385dc2c9033438ff453c5835f2b327019e2
Chunmao-Zhang <853015488@qq.com>
2025-12-16 13:41:34 +0800

    add evaluation

--- script/generate_report_zcm.py
+++ script/generate_report_zcm.py
@@ -27,7 +27,7 @@ def get_args() -> tuple[Namespace, dict]:
     parser.add_argument(
         "--ids",
         type=str,
-        default="50",
+        default="300",
     )
     parser.add_argument(
         "--output",
